f759b68a404acfdd2e19c7476f14a45792e42959
Bakhir Andrey <bakhir.a.d.st@gmail.com>
2020-05-25 19:40:17 +0300

    square implemented

---
+++ src_08/Square.py
@@ -0,0 +1,31 @@
+from src_08.Rectangle import Rectangle
+from src_08.Rhomb import Rhomb
+from typing import *
+from PyQt5.QtCore import QPoint, QLineF
+
+
+class Square(Rectangle, Rhomb):
+    def __init__(self, points: List[QPoint]):
+        Rectangle.__init__(self, points)
+        Rhomb.__init__(self, points)
+
+    def area(self) -> float:
+        a = self.edges[0]
+        return a**2 
+
+    def perimeter(self) -> float:
+        a = self.edges[0]
+        return 4*a 
+
+
+if __name__ == "__main__":
+    points = [
+        QPoint( 0,  0),
+        QPoint( 0, 30),
+        QPoint(30, 30),
+        QPoint(30,  0),
+    ]
+    a = Square(points)
+    print('Периметр: ', a.perimeter())
+    print('Площадь: ', a.area())
+
